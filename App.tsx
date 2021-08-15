516320bd1b1188a1c547b63ea19619fc9d9d9cab
Santiago <santiago.gonzalez.siordia@gmail.com>
2021-08-15 15:39:15 -0500

    feat: SafeAreaView added

--- App.tsx
+++ App.tsx
@@ -1,9 +1,14 @@
 import React, { FC } from 'react';
+import 'react-native-gesture-handler';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { MainStackNavigator } from './src/features/navigation/MainStackNavigator';
+
 
 const App: FC = () => {
   return (
-    <MainStackNavigator />
+    <SafeAreaView style={{ flex: 1 }}>
+      <MainStackNavigator />
+    </SafeAreaView>
   );
 }
 
