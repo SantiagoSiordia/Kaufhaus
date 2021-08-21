96a5b41174e74b280854b96f8244f6bf3dbe25c7
Santiago <santiago.gonzalez.siordia@gmail.com>
2021-08-21 14:02:26 -0500

    feat: logo on Header.tsx

--- src/features/header/Header.tsx
+++ src/features/header/Header.tsx
@@ -1,10 +1,29 @@
 import { NativeStackHeaderProps } from '@react-navigation/native-stack/lib/typescript/src/types';
 import React, { FC } from 'react';
-import { Text, View } from 'react-native';
+import { Image, StyleSheet, View } from 'react-native';
+import { Colors } from "../../theme/colors";
 
-export const Header: FC<NativeStackHeaderProps> = (props) => {
-    console.log(props)
-    return <View>
-        <Text>Header</Text>
+export const Header: FC<NativeStackHeaderProps> = () => {
+    return <View style={styles.container}>
+        {/* <Text style={styles.text}>Kaufhaus</Text> */}
+        <Image style={styles.logo} source={require('../../resources/images/Kaufhaus-logo.jpeg')} />
     </View>
 }
+
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: Colors.secondary,
+        padding: 16
+    },
+    text: {
+        fontSize: 16,
+        color: Colors.white,
+        fontFamily: 'TrebuchetMS-Bold'
+    },
+    logo: {
+        width: 20,
+        height: 20
+    }
+})
