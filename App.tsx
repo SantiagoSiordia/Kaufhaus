58acc295279c29a87e617724488309467f97ecfe
Santiago <santiago.gonzalez.siordia@gmail.com>
2021-08-21 22:48:59 -0500

    theme: Provider wrapping app

--- App.tsx
+++ App.tsx
@@ -1,14 +1,17 @@
 import React, { FC } from 'react';
 import 'react-native-gesture-handler';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { MainStackNavigator } from "./src/features/navigation/MainStackNavigator";
+import { MainStackNavigator } from './src/features/navigation/MainStackNavigator';
+import { KhThemeProvider } from './src/theme';
 
 const App: FC = () => {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <MainStackNavigator />
-    </SafeAreaView>
+    <KhThemeProvider>
+      <SafeAreaView style={{ flex: 1 }}>
+        <MainStackNavigator />
+      </SafeAreaView>
+    </KhThemeProvider>
   );
-}
+};
 
 export default App;
