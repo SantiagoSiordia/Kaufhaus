898ca94a57b38b0d18e00d65168372a04f21d681
Santiago <santiago.gonzalez.siordia@gmail.com>
2021-08-21 21:00:52 -0500

    feat: header

--- src/features/header/Header.tsx
+++ src/features/header/Header.tsx
@@ -1,29 +1,62 @@
 import { NativeStackHeaderProps } from '@react-navigation/native-stack/lib/typescript/src/types';
 import React, { FC } from 'react';
 import { Image, StyleSheet, View } from 'react-native';
-import { Colors } from "../../theme/colors";
+import { Colors } from '../../theme/colors';
+import { LinearGradient, LinearGradientProps } from 'expo-linear-gradient';
+
+type PickedGradient = Pick<
+  LinearGradientProps,
+  'colors' | 'start' | 'end' | 'locations'
+>;
+
+const horizontalGradient: PickedGradient = {
+  colors: [Colors.black, Colors.overlaySecondary],
+  end: { x: 1, y: 1 },
+  start: { x: 0, y: 0 },
+  locations: [0.13, 1],
+};
+
+const verticalGradient: PickedGradient = {
+  colors: [Colors.secondary, Colors.secondaryLight],
+  end: { x: 0, y: 1 },
+  start: { x: 0, y: 0 },
+  locations: [0, 1],
+};
 
 export const Header: FC<NativeStackHeaderProps> = () => {
-    return <View style={styles.container}>
-        {/* <Text style={styles.text}>Kaufhaus</Text> */}
-        <Image style={styles.logo} source={require('../../resources/images/Kaufhaus-logo.jpeg')} />
-    </View>
-}
+  return (
+    <LinearGradient {...verticalGradient}>
+      <LinearGradient {...horizontalGradient}>
+        <View style={{ padding: 8 }}>
+          <Image
+            resizeMode="contain"
+            style={styles.logo}
+            source={require('../../resources/images/Kaufhaus-logo.jpeg')}
+          />
+        </View>
+      </LinearGradient>
+    </LinearGradient>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: Colors.secondary,
-        padding: 16
-    },
-    text: {
-        fontSize: 16,
-        color: Colors.white,
-        fontFamily: 'TrebuchetMS-Bold'
-    },
-    logo: {
-        width: 20,
-        height: 20
-    }
-})
+  container: {
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    backgroundColor: Colors.black,
+    padding: 16,
+    flexDirection: 'row',
+  },
+  text: {
+    fontSize: 16,
+    color: Colors.white,
+    fontFamily: 'TrebuchetMS-Bold',
+  },
+  logo: {
+    width: 80,
+    height: 48,
+  },
+  flex1: {
+    width: '50%',
+  },
+});
