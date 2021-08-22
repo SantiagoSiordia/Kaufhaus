7982a546c2f9e0c384d49e298d0da5cff14cddba
Santiago <santiago.gonzalez.siordia@gmail.com>
2021-08-21 22:48:38 -0500

    theme: purple color

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -5,10 +5,13 @@ export const Colors = {
   white: '#FFFFFF',
   black: '#000000',
   violet: '#261542',
+  purple: '#533289',
+
   overlayPrimary: 'rgba(44, 14, 55, 0.3)',
   overlaySecondary: 'rgba(144, 4, 159, 0.3)',
   overlaySecondaryLight: 'rgba(255, 194, 226, 0.3)',
   overlayWhite: 'rgba(255, 255, 255, 0.3)',
   overlayBlack: 'rgba(0, 0, 0, 0.3)',
   overlayViolet: 'rgba(38, 21, 66, 0.3)',
+  overLayPurple: 'rgba(83, 50, 137, 0.3)',
 } as const;
