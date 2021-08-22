d802bfdde6eeca487a8f52e325c6f2e1b2a63ae8
Santiago <santiago.gonzalez.siordia@gmail.com>
2021-08-21 21:00:43 -0500

    feat: overlay colors

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -1,7 +1,12 @@
 export const Colors = {
-    primary: '#2C0E37',
-    secondary: '#90049F',
-    secondaryLight: '#FFC2E2',
-    white: '#FFFFFF',
-    black: '#000000'
-}
+  primary: '#2C0E37',
+  secondary: '#90049F',
+  secondaryLight: '#FFC2E2',
+  white: '#FFFFFF',
+  black: '#000000',
+  overlayPrimary: 'rgba(44, 14, 55, 0.3)',
+  overlaySecondary: 'rgba(144, 4, 159, 0.3)',
+  overlaySecondaryLight: 'rgba(255, 194, 226, 0.3)',
+  overlayWhite: 'rgba(255, 255, 255, 0.3)',
+  overlayBlack: 'rgba(0, 0, 0, 0.3)',
+};
