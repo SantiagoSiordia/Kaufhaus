b1a8703e8f43f6ad97ffabcd15ff7c3092a08260
Santi Siordia <santiago.gonzalez.siordia@gmail.com>
2021-10-04 17:36:39 -0500

    feat: totalprice of list in redux

--- src/features/redux/reducers/checklist.ts
+++ src/features/redux/reducers/checklist.ts
@@ -3,10 +3,12 @@ import { CheckListItemType } from '../../checklist/components';
 
 interface ChecklistState {
     list: Array<CheckListItemType>
+    totalPrice: number
 };
 
 const initialState: ChecklistState = {
     list: [],
+    totalPrice: 0,
 };
 
 export const checklistSlice = createSlice({
@@ -18,11 +20,17 @@ export const checklistSlice = createSlice({
         ...state.list,
         action.payload
       ]
+      state.totalPrice += action.payload.price
     },
+    deleteItemFromChecklist: (state, action: PayloadAction<string>) => {
+      const listItemToDelete = state.list.find(item => item.id === action.payload);
+      state.list = state.list.filter(item => item.id !== action.payload);
+      state.totalPrice -= listItemToDelete?.price || 0;
+    }
   },
 })
 
 // Action creators are generated for each case reducer function
-export const { addItemToChecklist } = checklistSlice.actions
+export const { addItemToChecklist, deleteItemFromChecklist } = checklistSlice.actions
 
 export const checklistReducer = checklistSlice.reducer
