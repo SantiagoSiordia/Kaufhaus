add5fe8ea7ff20d07491775c895719ab3f0f7521
Santi Siordia <santiago.gonzalez.siordia@gmail.com>
2021-10-03 22:58:56 -0500

    style: checklist view

--- src/features/checklist/screens/CheckListScreen.tsx
+++ src/features/checklist/screens/CheckListScreen.tsx
@@ -1,10 +1,25 @@
-import React, { FC } from 'react';
+import { Button, Input } from "native-base";
+import React, { FC, useState } from 'react';
 import { KhText, KhView } from '~/src/components';
+import { KhContainer } from "~/src/components/KhContainer/KhContainer";
 
 export const CheckListScreen: FC = () => {
+
+  const [ item, setItem ] = useState<string>('');
+
+  const addItemToList = () => {
+    console.log('Hello');
+  }
+
   return (
     <KhView backgroundColor={'white'} flex={1}>
-      <KhText variant={'h2'}>This is the checklist screen!</KhText>
+      <KhView>
+        <KhContainer>
+          <KhText variant={'body-bold'}>This is the checklist screen!</KhText>
+          <Input onChangeText={setItem} value={item} size="md" placeholder="¡Inserte nuevo elemento!"/>
+          <Button onPress={addItemToList}>Button</Button>
+        </KhContainer>
+      </KhView>
     </KhView>
   );
 };
