edcb197f7da6471b58b96d99c4305c9eb234a94f
Santi Siordia <santiago.gonzalez.siordia@gmail.com>
2021-10-04 00:08:59 -0500

    feat: KhButton!

--- src/components/KhButton/KhButton.tsx
+++ src/components/KhButton/KhButton.tsx
@@ -1,10 +1,17 @@
-import { Button } from 'native-base';
+import { Button, IButtonProps } from 'native-base';
 import React, { FC } from 'react';
 
-export const KhButton: FC = () => {
+export interface KhButtonProps extends IButtonProps{
+    text: string;
+}
+
+export const KhButton: FC<KhButtonProps> = ({
+    text,
+    ...buttonProps
+}) => {
     return (
-        <Button>
-            Hello
+        <Button {...buttonProps}>
+            {text}
         </Button>
     )
 }
